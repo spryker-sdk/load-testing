de09ff327a80e26f2de3acc86021c9b586d7fde3
Yuliia Sakharova <yuliia.sakharova@spryker.com>
2021-08-23 12:27:54 +0300

    MP-5688: Add setting price and localized attributes while product creation.

--- tests/SprykerSdkTest/LoadTesting/Fixtures/ProductConcrete/ProductConcreteFixtures.php
+++ tests/SprykerSdkTest/LoadTesting/Fixtures/ProductConcrete/ProductConcreteFixtures.php
@@ -7,9 +7,24 @@
 
 namespace SprykerSdkTest\LoadTesting\Fixtures\ProductConcrete;
 
+use Codeception\Configuration;
+use Generated\Shared\DataBuilder\LocalizedAttributesBuilder;
+use Generated\Shared\Transfer\LocaleTransfer;
+use Generated\Shared\Transfer\LocalizedAttributesTransfer;
 use Generated\Shared\Transfer\ProductAbstractTransfer;
 use Generated\Shared\Transfer\ProductConcreteTransfer;
+use Orm\Zed\Locale\Persistence\SpyLocaleQuery;
 use Propel\Runtime\Propel;
+use Spryker\Zed\PriceProduct\Communication\Plugin\ProductConcrete\PriceProductConcreteAfterCreatePlugin;
+use Spryker\Zed\ProductBundle\Communication\Plugin\Product\ProductBundleProductConcreteAfterCreatePlugin;
+use Spryker\Zed\ProductDiscontinuedProductBundleConnector\Communication\Plugin\Product\DiscontinuedProductConcreteAfterCreatePlugin;
+use Spryker\Zed\ProductImage\Communication\Plugin\ProductConcreteAfterCreatePlugin as ImageSetProductConcreteAfterCreatePlugin;
+use Spryker\Zed\ProductSearch\Communication\Plugin\ProductConcrete\ProductSearchProductConcreteAfterCreatePlugin;
+use Spryker\Zed\ProductSearch\Communication\Plugin\ProductConcrete\ProductSearchProductConcreteAfterUpdatePlugin;
+use Spryker\Zed\ProductSearch\Communication\Plugin\ProductConcrete\ProductSearchProductConcreteReadPlugin;
+use Spryker\Zed\ProductSearch\Communication\Plugin\ProductSearchConfigExtensionCollectorPlugin;
+use Spryker\Zed\ProductValidity\Communication\Plugin\ProductValidityCreatePlugin;
+use Spryker\Zed\Stock\Communication\Plugin\ProductConcreteAfterCreatePlugin as StockProductConcreteAfterCreatePlugin;
 use SprykerSdkTest\LoadTesting\Fixtures\Helper\LoadTestingCsvDemoDataLoaderTrait;
 use SprykerSdkTest\LoadTesting\Fixtures\LoadTestingProductTester;
 use SprykerTest\Shared\Testify\Fixtures\FixturesBuilderInterface;
@@ -23,6 +38,17 @@ use SprykerTest\Shared\Testify\Fixtures\FixturesContainerInterface;
 class ProductConcreteFixtures implements FixturesBuilderInterface, FixturesContainerInterface
 {
     use LoadTestingCsvDemoDataLoaderTrait;
+
+    protected const IS_SUPER_ATTRIBUTE_KEY = 'is_super';
+    protected const ATTRIBUTE_KEY = 'key';
+
+    protected const FORMAT_SUPER_ATTRIBUTE = 'attribute_key_%s';
+    protected const FORMAT_SUPER_ATTRIBUTE_VALUE = 'attribute_value_%s';
+
+    protected const ATTRIBUTE_MAP_KEY = 'attribute_map';
+    protected const ATTRIBUTE_VARIANTS_KEY = 'attribute_variants';
+
+    protected const ATTRIBUTE_KEY_COLOR = 'color';
 
     /**
      * @param \SprykerSdkTest\LoadTesting\Fixtures\LoadTestingProductTester $I
@@ -41,39 +67,57 @@ class ProductConcreteFixtures implements FixturesBuilderInterface, FixturesConta
         return $this;
     }
 
+    /**
+     * @param \SprykerSdkTest\LoadTesting\Fixtures\LoadTestingProductTester $I
+     *
+     * @return void
+     */
     protected function createProductConcrete(LoadTestingProductTester $I): void
     {
         $demoData = $this->loadDemoData();
-
         $productFacade = $I->getLocator()->product()->facade();
 
         foreach ($demoData as $data) {
-            try {
+            try{
                 $sku = $data['sku'];
-
                 if ($productFacade->hasProductConcrete($sku)) {
                     continue;
                 }
 
+                $productAttributes[static::ATTRIBUTE_KEY_COLOR] = sprintf(static::FORMAT_SUPER_ATTRIBUTE_VALUE, $sku);
+
                 $productConcreteOverride = [
                     ProductConcreteTransfer::SKU => $sku,
                     ProductConcreteTransfer::IS_ACTIVE => 1,
+                    ProductConcreteTransfer::ATTRIBUTES => $productAttributes
+                ];
+                $productAbstractOverride = [
+                    ProductAbstractTransfer::SKU => $data['abstract_sku'],
+                    ProductAbstractTransfer::ATTRIBUTES => $productAttributes
                 ];
 
-                $I->haveFullProductWithPrice($productConcreteOverride, [
-                    ProductAbstractTransfer::SKU => $data['abstract_sku'],
-                ], $data['pdp_url']);
+                $productConcreteTransfer = $I->haveFullProductWithPrice(
+                    $productConcreteOverride,
+                    $productAbstractOverride,
+                    $data['pdp_url']
+                );
             } catch (\Throwable $e) {
                 echo $e->getMessage() . PHP_EOL;
             }
         }
     }
 
+    /**
+     * @return string
+     */
     protected function getFileName(): string
     {
         return 'product_concrete_2.csv';
     }
 
+    /**
+     * @return string[]
+     */
     protected function getRequiredFields(): array
     {
         return [
--- tests/SprykerSdkTest/LoadTesting/Fixtures/_support/LoadTestingProductTester.php
+++ tests/SprykerSdkTest/LoadTesting/Fixtures/_support/LoadTestingProductTester.php
@@ -8,14 +8,25 @@
 namespace SprykerSdkTest\LoadTesting\Fixtures;
 
 use Codeception\Actor;
+use Generated\Shared\DataBuilder\PriceProductBuilder;
+use Generated\Shared\Transfer\CurrencyTransfer;
 use Generated\Shared\Transfer\MoneyValueTransfer;
+use Generated\Shared\Transfer\PriceProductDimensionTransfer;
 use Generated\Shared\Transfer\PriceProductTransfer;
+use Generated\Shared\Transfer\PriceTypeTransfer;
+use Generated\Shared\Transfer\ProductAbstractTransfer;
 use Generated\Shared\Transfer\ProductConcreteTransfer;
 use Generated\Shared\Transfer\StockProductTransfer;
+use Generated\Shared\Transfer\StoreTransfer;
 use Orm\Zed\Url\Persistence\SpyUrlQuery;
+use Spryker\Shared\PriceProduct\PriceProductConfig;
+use Spryker\Zed\Currency\Business\CurrencyFacadeInterface;
+use Spryker\Zed\PriceProduct\Business\PriceProductFacadeInterface;
+use Spryker\Zed\PriceProduct\Persistence\PriceProductQueryContainerInterface;
 use Spryker\Zed\Store\Business\StoreFacadeInterface;
 use SprykerTest\Shared\Testify\Fixtures\FixturesExporterInterface;
 use SprykerTest\Shared\Testify\Fixtures\FixturesTrait;
+use SprykerTest\Shared\Testify\Helper\DataCleanupHelperTrait;
 
 /**
  * Inherited Methods
@@ -37,6 +48,12 @@ class LoadTestingProductTester extends Actor implements FixturesExporterInterfac
 {
     use _generated\LoadTestingProductTesterActions;
     use FixturesTrait;
+
+    protected const EUR_ISO_CODE = 'EUR';
+    protected const NET_PRICE = 10;
+    protected const GROSS_PRICE = 9;
+    protected const STORE_NAME_DE = "DE";
+    protected const STORE_NAME_US = "US";
 
     /**
      * @param array $productConcreteOverride
@@ -60,7 +77,7 @@ class LoadTestingProductTester extends Actor implements FixturesExporterInterfac
             StockProductTransfer::IS_NEVER_OUT_OF_STOCK => 1,
         ]);
 
-        $priceProductOverride = [
+        $priceProductConcreteOverride = [
             PriceProductTransfer::SKU_PRODUCT_ABSTRACT => $productConcreteTransfer->getAbstractSku(),
             PriceProductTransfer::SKU_PRODUCT => $productConcreteTransfer->getSku(),
             PriceProductTransfer::ID_PRODUCT => $productConcreteTransfer->getIdProductConcrete(),
@@ -70,12 +87,154 @@ class LoadTestingProductTester extends Actor implements FixturesExporterInterfac
                 MoneyValueTransfer::GROSS_AMOUNT => 880,
             ],
         ];
+        $priceProductAbstractOverride = [
+            PriceProductTransfer::SKU_PRODUCT_ABSTRACT => $productConcreteTransfer->getAbstractSku(),
+            PriceProductTransfer::SKU_PRODUCT => $productConcreteTransfer->getAbstractSku(),
+            PriceProductTransfer::ID_PRODUCT => $productConcreteTransfer->getFkProductAbstract(),
+            PriceProductTransfer::PRICE_TYPE_NAME => 'DEFAULT',
+            PriceProductTransfer::MONEY_VALUE => [
+                MoneyValueTransfer::NET_AMOUNT => 770,
+                MoneyValueTransfer::GROSS_AMOUNT => 880,
+            ],
+        ];
 
-        $this->havePriceProduct($priceProductOverride);
+        $this->havePriceProduct($priceProductConcreteOverride);
+        $this->createPriceProductAbstract($productConcreteTransfer->getFkProductAbstract(),$priceProductAbstractOverride);
         $this->replaceProductUrl($productConcreteTransfer->getFkProductAbstract(), $productUrl);
         $this->haveAvailabilityAbstract($productConcreteTransfer);
 
         return $productConcreteTransfer;
+    }
+
+    /**
+     * @param int $idProductAbstract
+     * @param array $priceProductOverride
+     *
+     * @return \Generated\Shared\Transfer\PriceProductTransfer
+     */
+    protected function createPriceProductAbstract(int $idProductAbstract, array $priceProductOverride = []): ProductAbstractTransfer
+    {
+        $priceProductTransfer1 = $this->createPriceProductTransfer(
+            $priceProductOverride,
+            static::NET_PRICE,
+            static::GROSS_PRICE,
+            static::EUR_ISO_CODE,
+            static::STORE_NAME_DE
+        );
+        $priceProductTransfer2 = $this->createPriceProductTransfer(
+            $priceProductOverride,
+            static::NET_PRICE,
+            static::GROSS_PRICE,
+            static::EUR_ISO_CODE,
+            static::STORE_NAME_US
+        );
+
+        $productAbstractTransfer = $this->getPriceProductFacade()->persistProductAbstractPriceCollection(
+            (new ProductAbstractTransfer())
+                ->setIdProductAbstract($idProductAbstract)
+                ->addPrice($priceProductTransfer1)
+                ->addPrice($priceProductTransfer2)
+        );
+
+        return $productAbstractTransfer;
+    }
+
+    /**
+     * @param array $priceProductOverride
+     * @param int $netPrice
+     * @param int $grossPrice
+     * @param string $currencyIsoCode
+     *
+     * @return \Generated\Shared\Transfer\PriceProductTransfer
+     */
+    protected function createPriceProductTransfer(
+        array $priceProductOverride,
+        int $netPrice,
+        int $grossPrice,
+        string $currencyIsoCode,
+        string $storeName
+    ): PriceProductTransfer {
+        $currencyTransfer = $this->getCurrencyFacade()->fromIsoCode($currencyIsoCode);
+
+        $config = $this->getSharedPriceProductConfig();
+
+        $priceDimensionTransfer = (new PriceProductDimensionTransfer())
+            ->setType($config->getPriceDimensionDefault());
+
+        $defaultPriceTypeName = $this->getPriceProductFacade()->getDefaultPriceTypeName();
+        $idPriceType = $this->getPriceTypeId($defaultPriceTypeName);
+
+        $priceTypeTransfer = (new PriceTypeTransfer())
+            ->setName($defaultPriceTypeName)
+            ->setIdPriceType($idPriceType);
+
+        if (isset($priceProductOverride[PriceProductTransfer::PRICE_TYPE])) {
+            $priceTypeTransfer = $priceProductOverride[PriceProductTransfer::PRICE_TYPE];
+        }
+
+        if (isset($priceProductOverride[PriceProductTransfer::MONEY_VALUE][MoneyValueTransfer::NET_AMOUNT])) {
+            $netPrice = $priceProductOverride[PriceProductTransfer::MONEY_VALUE][MoneyValueTransfer::NET_AMOUNT];
+        }
+
+        if (isset($priceProductOverride[PriceProductTransfer::MONEY_VALUE][MoneyValueTransfer::GROSS_AMOUNT])) {
+            $grossPrice = $priceProductOverride[PriceProductTransfer::MONEY_VALUE][MoneyValueTransfer::GROSS_AMOUNT];
+        }
+
+        if (isset($priceProductOverride[PriceProductTransfer::MONEY_VALUE][MoneyValueTransfer::CURRENCY])) {
+            $currencyTransfer = $priceProductOverride[PriceProductTransfer::MONEY_VALUE][MoneyValueTransfer::CURRENCY];
+        }
+
+        $priceProductDefaultData = [
+            PriceProductTransfer::PRICE_TYPE_NAME => $priceTypeTransfer->getName(),
+            PriceProductTransfer::PRICE_TYPE => $priceTypeTransfer,
+            PriceProductTransfer::PRICE_DIMENSION => $priceDimensionTransfer,
+            PriceProductTransfer::FK_PRICE_TYPE => $priceTypeTransfer->getIdPriceType(),
+        ];
+
+        $priceProductTransfer = (new PriceProductBuilder($priceProductDefaultData))
+            ->seed($priceProductOverride)
+            ->build();
+
+        $storeTransfer = $this->getStoreFacade()->getStoreByName($storeName);
+
+        $moneyValueTransfer = $this->createMoneyValueTransfer(
+            $grossPrice,
+            $netPrice,
+            $storeTransfer,
+            $currencyTransfer,
+            $priceProductOverride[PriceProductTransfer::MONEY_VALUE] ?? []
+        );
+
+        $priceProductTransfer->setMoneyValue($moneyValueTransfer);
+
+        return $priceProductTransfer;
+    }
+
+    /**
+     * @param int $grossAmount
+     * @param int $netAmount
+     * @param \Generated\Shared\Transfer\StoreTransfer $storeTransfer
+     * @param \Generated\Shared\Transfer\CurrencyTransfer $currencyTransfer
+     * @param array $moneyValueOverride
+     *
+     * @return \Generated\Shared\Transfer\MoneyValueTransfer
+     */
+    protected function createMoneyValueTransfer(
+        int $grossAmount,
+        int $netAmount,
+        StoreTransfer $storeTransfer,
+        CurrencyTransfer $currencyTransfer,
+        array $moneyValueOverride = []
+    ): MoneyValueTransfer {
+        $moneyValueTransfer = (new MoneyValueTransfer())->fromArray($moneyValueOverride, true);
+
+        return $moneyValueTransfer
+            ->setNetAmount($netAmount)
+            ->setGrossAmount($grossAmount)
+            ->setFkStore($storeTransfer->getIdStore())
+            ->setFkCurrency($currencyTransfer->getIdCurrency())
+            ->setCurrency($currencyTransfer)
+            ->setStore($storeTransfer);
     }
 
     /**
@@ -103,5 +262,53 @@ class LoadTestingProductTester extends Actor implements FixturesExporterInterfac
     protected function getStoreFacade(): StoreFacadeInterface
     {
         return $this->getLocator()->store()->facade();
+    }
+
+    /**
+     * @return \Spryker\Zed\PriceProduct\Business\PriceProductFacadeInterface
+     */
+    protected function getPriceProductFacade(): PriceProductFacadeInterface
+    {
+        return $this->getLocator()->priceProduct()->facade();
+    }
+
+    /**
+     * @return \Spryker\Zed\Currency\Business\CurrencyFacadeInterface
+     */
+    protected function getCurrencyFacade(): CurrencyFacadeInterface
+    {
+        return $this->getLocator()->currency()->facade();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return int|null
+     */
+    protected function getPriceTypeId(string $name): ?int
+    {
+        $spyPriceTypeEntity = $this->getPriceProductQueryContainer()->queryPriceType($name)->findOne();
+
+        if (!$spyPriceTypeEntity) {
+            return null;
+        }
+
+        return $spyPriceTypeEntity->getIdPriceType();
+    }
+
+    /**
+     * @return \Spryker\Zed\PriceProduct\Persistence\PriceProductQueryContainerInterface
+     */
+    protected function getPriceProductQueryContainer(): PriceProductQueryContainerInterface
+    {
+        return $this->getLocator()->priceProduct()->queryContainer();
+    }
+
+    /**
+     * @return \Spryker\Shared\PriceProduct\PriceProductConfig
+     */
+    protected function getSharedPriceProductConfig(): PriceProductConfig
+    {
+        return new PriceProductConfig();
     }
 }

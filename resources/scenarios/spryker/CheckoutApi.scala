0f948a002b95d0ecacb8726d352ea189fa98f781
Kartav1K <roman.varkuta@gmail.com>
2021-09-03 12:13:55 +0300

    Remove access token request from checkout scenarios

--- resources/scenarios/spryker/CheckoutApi.scala
+++ resources/scenarios/spryker/CheckoutApi.scala
@@ -31,13 +31,6 @@ trait CheckoutApiBase {
     val productFeeder = csv("tests/_data/product_concrete.csv").random
     val customerFeeder = csv("tests/_data/customer.csv").random
 
-    val getAccessTokenRequest = http("Get Access Token")
-        .post("/access-tokens")
-        .header("Content-Type", "application/json")
-        .body(StringBody("""{"data": {"type": "access-tokens", "attributes": {"username": "${email}", "password": "${password}"}}}"""))
-        .check(status.is(201))
-        .check(jsonPath("$.data.attributes.accessToken").saveAs("auth_token"))
-
     val getCartRequest = http("Get Cart Request")
         .get("/carts")
         .header("Authorization", "Bearer ${auth_token}")
@@ -63,7 +56,6 @@ trait CheckoutApiBase {
         .feed(customerFeeder)
         .repeat(1) {
             feed(productFeeder)
-                .exec(getAccessTokenRequest)
                 .exec(getCartRequest)
                 .exec(addToCartRequest)
                 .exec(checkoutRequest)

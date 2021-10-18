f0ef6a8c775a4e29aeb4ea0adc89a6bf40f0656e
andrew-pushkar <andrew-pushkar@bitbucket.com>
2021-10-18 17:33:56 +0300

    * changed scenarios order and removed unnecessary ones

--- resources/scenarios/scenarios.js
+++ resources/scenarios/scenarios.js
@@ -1,22 +1,9 @@
 const scenarios = [
     {
-        title: 'Storefront',
-        tests: [
-            { id: 'Home', title: 'Home page'},
-            { id: 'Nope', title: 'Empty index'},
-            { id: 'CatalogSearch', title: 'Search page'},
-            { id: 'Pdp', title: 'PDP page'},
-            { id: 'AddToGuestCart', title: 'Add to guest cart request'},
-            { id: 'AddToCustomerCart', title: 'Add to customer cart request'},
-            { id: 'PlaceOrder', title: 'Place order request'},
-            { id: 'PlaceOrderCustomer', title: 'Place order customer request'},
-        ],
-    },
-    {
         title: 'API',
         tests: [
+            { id: 'CatalogSearchProductOffersApi', title: 'Catalog Search Product Offers'},
             { id: 'PdpApi', title: 'PDP request'},
-            { id: 'CatalogSearchApi', title: 'Search request'},
             { id: 'CheckoutApi', title: 'Checkout request'},
             { id: 'CheckoutWithLessOrEqualFiftyItemsApi', title: 'Checkout With Less Or Equal Fifty Items request'},
             { id: 'CheckoutWithFiftyToSeventyItemsApi', title: 'Checkout With Fifty To Seventy Items request'},
@@ -26,12 +13,6 @@ const scenarios = [
             { id: 'AddItemToCartApi', title: 'Add item to cart'},
             { id: 'AddItemToCartWithLessOrEqualFiftyItemsApi', title: 'Add Item To Cart with <= 50 items'},
             { id: 'AddItemToCartWithFiftyToSeventyItemsApi', title: 'Add Item To Cart with >= 50 & <= 70 items'},
-            { id: 'HomeApi', title: 'Home request'},
-            { id: 'CatalogSearchProductOffersApi', title: 'Catalog Search Product Offers'},
-            { id: 'ProductOffersForProductsConcreteApi', title: 'Product Offers For Products Concrete'},
-            { id: 'CartApi', title: 'Add to cart request'},
-            { id: 'GuestCartApi', title: 'Add to guest cart request'},
-            { id: 'GuestCheckoutApi', title: 'Guest checkout request'},
         ],
     },
 ];

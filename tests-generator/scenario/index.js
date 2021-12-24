46441e71618539a50cf88ada2607e6a73628707d
andrew-pushkar <andrew-pushkar@bitbucket.com>
2021-12-24 15:11:39 +0200

    Created functionality for skipping endpoints

--- tests-generator/scenario/index.js
+++ tests-generator/scenario/index.js
@@ -5,6 +5,10 @@ const scenariosList = require('../scenarios-list');
 
 exports.generateGatlingTestFile = (endpoint, data, schemas, type) => {
     for (let method in data) {
+        if (data[method].hasOwnProperty('skip') && data[method].skip === true) {
+            continue;
+        }
+
         const classContentFile = classContent.build(data[method], method, endpoint, schemas, type);
         const className = classContent.generateClassName(data[method].operationId, method, endpoint, type);
         const classFullPath = process.cwd() + `/resources/scenarios/spryker/${className}.scala`;
